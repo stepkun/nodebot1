7bf200582649ee78c7a0952cc264f812819dea2c
stepkun <stephan.kunz@kabelbw.de>
2022-04-09 16:49:14 +0200

    naming convention

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# change into parent src dir
+# change into parent dir (src of ros2 workspace)
 cd ..
 # install pico-chassis and its dependencies
 if [ -d pico-chassis ]
 then
-    echo "The pico-chassis is already installed"
-    echo "To reinstall remove directory pico-chassis in your ros2 workspace src directory"
+    echo "The pico_chassis is already installed"
+    echo "To reinstall remove directory pico_chassis in your ros2 workspace src directory"
 else
-    git clone git@github.com:stepkun/pico-chassis.git
-    cd pico-chassis
+    git clone git@github.com:stepkun/pico_chassis.git
+    cd pico_chassis
     ./setup_micro-ros-pico-sdk.sh
     ./setup_micro-ros-agent.sh
     cd ..
